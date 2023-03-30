5fb38eb2b1a93467cc12458c1e6b77e56230c505
onlyVishesh <Visheshmaurya14@gmail.com>
2023-03-30 21:39:58 +0530

    folder Updated

---
+++ Random/timepass.py
@@ -0,0 +1,6 @@
+n = int(input())
+if n == 4:
+    print(" *")
+    print("/ \\")
+    print("/ \\")
+    print(" *")
--- Random/turtleFlower.py
+++ Random/turtleFlower.py
@@ -1,40 +1,34 @@
 from turtle import *
 
 ht()
-speed(0)
+speed()
 pensize(1.2)
 shape('turtle')
 
-
-
 for i in range(24):
-   for j in range(6):
-      left(60)
-      
-   
-      fillcolor('cyan')
-      up()
-      setpos(0,0)
-      down()
-      begin_fill()
-      circle((600-(i*25)),60)
-      up()
-      setpos(0,0)
-      down()
-      circle(-(600-(i*25)),60)
-      end_fill()
-   
-      
-      fillcolor('yellow')
-      up()
-      setpos(0,0)
-      down()
-      begin_fill()
-      circle((587.5-(i*25)),60)
-      up()
-      setpos(0,0)
-      down()
-      circle(-(587.5-(i*25)),60)
-      end_fill()
-      
+    for j in range(6):
+        left(60)
+
+        fillcolor('cyan')
+        up()
+        setpos(0,0)
+        down()
+        circle((600-(i*25)),60)
+        up()
+        setpos(0,0)
+        down()
+        circle(-(600-(i*25)),60)
+        end_fill()
+
+        fillcolor('yellow')
+        up()
+        setpos(0,0)
+        down()
+        circle((587.5-(i*25)),60)
+        up()
+        setpos(0,0)
+        down()
+        circle(-(587.5-(i*25)),60)
+        end_fill()
+
 mainloop()
